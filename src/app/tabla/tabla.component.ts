e4443d81686a20a4bd6370b28064e1a5c9fc7919
achozaslopez <122780568+achozaslopez@users.noreply.github.com>
2023-02-17 14:53:43 +0100

    commit

---
+++ src/app/tabla/tabla.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { ConexionService } from 'src/conexion.service';
+import { Categoria } from '../interface/categoria';
+
+@Component({
+  selector: 'app-tabla',
+  templateUrl: './tabla.component.html',
+  styleUrls: ['./tabla.component.css']
+})
+export class TablaComponent {
+
+  listado: Categoria[] = [];
+  constructor(private conexiom:ConexionService){
+    const dato:Observable<any>= this.conexion.leerApi('categorias');
+  }
+
+}
