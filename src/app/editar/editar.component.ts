f2f49c84a839edf82e859b56e1d7654e22f4f75c
achozaslopez <122780568+achozaslopez@users.noreply.github.com>
2023-02-17 14:50:30 +0100

    commit

---
+++ src/app/editar/editar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { Dato, ConexionService} from 'src/conexion.service'
+import { ActivatedRoute, Route } from '@angular/router';
+
+@Component({
+  selector: 'app-editar',
+  templateUrl: './editar.component.html',
+  styleUrls: ['./editar.component.css']
+})
+export class EditarComponent {
+
+}
